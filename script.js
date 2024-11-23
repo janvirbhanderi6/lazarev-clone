ce813dd10f5a9a3ef16cb5c7f14b328c1e9e4a07
janvi <janvibhanderi460@gmail.com>
2024-11-23 17:32:34 +0530

    page 3,4,5 added

--- script.js
+++ script.js
@@ -38,4 +38,62 @@ nav.addEventListener("mouseleave", function(){
 })
 }
 
-navAnimation()
+function page2Animation() {
+    var rightElems = document.querySelectorAll(".right-elem");
+
+rightElems.forEach(function(elem) {
+    elem.addEventListener("mouseenter", function() {
+        gsap.to(elem.childNodes[3], {
+            duration: 0.5,
+            opacity: 0.9,
+            scale: 1
+        })
+    });
+    elem.addEventListener("mouseleave", function() {
+        gsap.to(elem.childNodes[3], {
+            duration: 0.5,
+            opacity: 0,
+            scale: 0
+        })
+    });
+    elem.addEventListener("mousemove", function(dets) {
+        gsap.to(elem.childNodes[3], {
+            x: dets.x - elem.getBoundingClientRect().x - 50,
+            y: dets.y - elem.getBoundingClientRect().y - 140
+        })
+    })
+})
+}
+
+function page3VideoAnimation() {
+    let page3Center = document.querySelector(".page3-center");
+let video = document.querySelector("#page3 video");
+
+page3Center.addEventListener("click", function() {
+    video.play();
+    gsap.to(video, {
+        Transform: "scaleX(1) scaleY(1)",
+        opacity: 1,
+        borderRadius: 0
+    })
+})
+
+video.addEventListener("click", function() {
+    video.pause();
+    gsap.to(video, {
+        Transform: "scaleX(0.6) scaleY(0)",
+        opacity: 0,
+        borderRadius: "30px"
+    })
+
+})
+
+video.addEventListener("pause", function() {
+    video.currentTime = 0;
+});
+}
+
+navAnimation();
+page2Animation();
+page3VideoAnimation();
+
